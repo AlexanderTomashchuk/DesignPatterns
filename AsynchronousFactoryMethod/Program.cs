fd3497a8c8a205be6e90d5d412465fa01d9ed884
Alexander Tomashchuk <alexandertomashchuk@gmail.com>
2020-02-02 02:53:14 +0200

    Created AsynchronousFactoryMethod project

---
+++ AsynchronousFactoryMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AsynchronousFactoryMethod
+{
+    internal class Foo
+    {
+        public string Data;
+
+        private Foo() {}
+
+        public static async Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            
+            return await result.InitAsync();
+        }
+        
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(2000);
+            Data = await Task.FromResult("Foo data");
+
+            return this;
+        } 
+    }
+    
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var foo = await Foo.CreateAsync();
+
+            Console.WriteLine(foo.Data);
+        }
+    }
+}
