5b8c03141ee61a651cba5938b2325325baa1b354
Alexander Tomashchuk <alexandertomashchuk@gmail.com>
2020-02-04 02:07:58 +0200

    Finished factory exercise

---
+++ FactoryExercise/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FactoryExercise
+{
+    internal class Person
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        private Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        
+        public static PersonFactory Factory => new PersonFactory();
+
+        internal class PersonFactory
+        {
+            private static int _currentId = 0; 
+            
+            public Person CreatePerson(string name)
+            {
+                return new Person(_currentId++, name);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}";
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person0 = Person.Factory.CreatePerson("Alex");
+            var person1 = Person.Factory.CreatePerson("Kate");
+            var person2 = Person.Factory.CreatePerson("Opanas");
+
+            var person3 = new Person.PersonFactory().CreatePerson("Onton");
+            
+            Console.WriteLine(person0);
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+        }
+    }
+}
