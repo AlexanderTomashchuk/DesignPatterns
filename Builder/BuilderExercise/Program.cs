18fd88c91cbd27c0c9c75d2cb60ae5ecb0cdee76
AlexanderTomashchuk <alexandertomashchuk@gmail.com>
2021-09-25 01:40:21 +0300

    reorganized folders #2 commit

---
+++ Builder/BuilderExercise/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderExercise
+{
+    internal class ClassItem
+    {
+        public string Name;
+        public List<FieldItem> Fields = new List<FieldItem>();
+        
+        private int IndentSize = 2;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            
+            sb.AppendLine($"public class {Name}");
+            sb.AppendLine("{");
+            foreach (var field in Fields)
+            {
+                sb.Append(new string(' ', IndentSize));
+                sb.AppendLine(field.ToString());
+            }
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+    }
+
+    internal class FieldItem
+    {
+        public string Type;
+        public string Name;
+        
+        public FieldItem(string name, string type)
+        {
+            Type = type;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"public {Type} {Name};";
+        }
+    }
+    
+    internal class CodeBuilder
+    {
+        private ClassItem _classItem = new ClassItem();
+        
+        public CodeBuilder(string className)
+        {
+            _classItem.Name = className;
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            _classItem.Fields.Add(new FieldItem(name, type));
+
+            return this;
+        }
+        
+        public override string ToString()
+        {
+            return _classItem.ToString();
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person")
+                .AddField("Name", "string")
+                .AddField("Age", "int");
+
+            Console.WriteLine(cb);
+        }
+    }
+}
---
+++ Builder/FacetedBuilder/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Security.Authentication;
+
+namespace FacetedBuilder
+{
+    internal class Person
+    {
+        //address
+        public string City, StreetAddress, Postcode;
+        
+        //employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(City)}: {City}, {nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    internal class PersonBuilder //facade
+    {
+        protected Person person = new Person();
+        
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public Person Create()
+        {
+            return person;
+        }
+    }
+
+    internal class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+        
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+    
+    internal class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+    
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+
+            var person = pb
+                .Works.At("EPAM").AsA("Engineer").Earning(100000)
+                .Lives.At("123 London Road").In("London").WithPostcode("022132")
+                .Create();
+            
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
