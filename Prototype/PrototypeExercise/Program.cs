38fc7b24184bdac98cb1ebb888f271b8d76db044
AlexanderTomashchuk <alexandertomashchuk@gmail.com>
2021-09-26 01:46:35 +0300

    prototype exercise

---
+++ Prototype/PrototypeExercise/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PrototypeExercise
+{
+    public class Point
+    {
+        public int X, Y;
+
+        public override string ToString()
+        {
+            return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            // todo
+            return new Line
+            {
+                Start = new Point{ X = Start.X, Y = Start.Y},
+                End = new Point {X = End.X, Y = End.Y}
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line1 = new Line { Start = new Point { X = 1, Y = 1 }, End = new Point { X = 2, Y = 2 } };
+            
+            var line2 = line1.DeepCopy();
+            line2.End = new Point { X = 3, Y = 3 };
+            
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
+
+            Console.ReadKey();
+        }
+    }
+}
