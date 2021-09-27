c74853f675251b5859aef0f775fc5ed7ef653c57
AlexanderTomashchuk <alexandertomashchuk@gmail.com>
2021-09-27 20:58:38 +0300

    thread safe singleton

---
+++ Singleton/ThreadSafeSingleton/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ThreadSafeSingleton
+{
+    interface IDatabase
+    {
+        decimal GetProductPrice(string name);
+    }
+
+    class SingletonDatabase : IDatabase
+    {
+        private readonly Dictionary<string, decimal> _products = new();
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Ctor init");
+            File.ReadAllLines("products.txt").ToList().ForEach(
+                line =>
+                {
+                    var data = line.Split("|");
+                    _products.Add(data[0], decimal.Parse(data[1]));
+                });
+        }
+        
+        public decimal GetProductPrice(string name)
+        {
+            return _products[name];
+        }
+
+        private static Lazy<SingletonDatabase> _instance = new (new SingletonDatabase());
+
+        public static SingletonDatabase Instance => _instance.Value;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productName = "Apple";
+            var productPrice = SingletonDatabase.Instance.GetProductPrice(productName);
+            Console.WriteLine($"Product '{productName}' has a price {productPrice}");
+
+            productName = "Orange";
+            productPrice = SingletonDatabase.Instance.GetProductPrice(productName);
+            Console.WriteLine($"Product '{productName}' has a price {productPrice}");
+            
+            Console.ReadKey();
+        }
+    }
+}
