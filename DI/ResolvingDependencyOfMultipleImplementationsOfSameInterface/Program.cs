d74bcf011046fb4bb3a8c25d4c95eb97f6e773c7
AlexanderTomashchuk <alexandertomashchuk@gmail.com>
2021-09-28 19:53:17 +0300

    resolving dependency og multiple implementations of same interface

---
+++ DI/ResolvingDependencyOfMultipleImplementationsOfSameInterface/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ResolvingDependencyOfMultipleImplementationsOfSameInterface
+{
+    interface IService
+    {
+        void TestMethod();
+    }
+
+    class ServiceA : IService
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("ServiceA");
+        }
+    }
+    
+    class ServiceB : IService
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("ServiceB");
+        }
+    }
+
+    class Consumer
+    {
+        private readonly IService _service;
+
+        public Consumer(ServiceResolver serviceResolver)
+        {
+            _service = serviceResolver("A");
+        }
+
+        public void Test()
+        {
+            _service.TestMethod();
+        }
+    }
+    
+    delegate IService ServiceResolver(string key);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<ServiceA>();
+            serviceCollection.AddTransient<ServiceB>();
+            serviceCollection.AddTransient<Consumer>();
+            serviceCollection.AddTransient<ServiceResolver>(serviceProvider => key =>
+            {
+                return key switch
+                {
+                    "A" => serviceProvider.GetService<ServiceA>(),
+                    "B" => serviceProvider.GetService<ServiceB>(),
+                    _ => throw new KeyNotFoundException()
+                };
+            });
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var consumer = serviceProvider.GetRequiredService<Consumer>();
+            
+            consumer.Test();
+            
+            Console.ReadKey();
+        }
+    }
+}
